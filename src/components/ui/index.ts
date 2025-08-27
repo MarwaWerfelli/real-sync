c277f2d9aa3b4cf6468c6284d82aa98dee07d17d
MarwaWerfelli <marwa.werfelli.developer@gmail.com>
2025-08-27 21:50:12 +0100

    Create base UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+// Export all UI components for easy importing
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Card, CardContent, CardHeader, CardTitle } from "./Card";
+export { LoadingSpinner } from "./LoadingSpinner";
